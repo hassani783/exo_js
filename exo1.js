b948450221234591ea8e44cce38a6290ea9f9f79
fzhass <fz.hassani783@gmail.com>
2021-01-28 11:32:17 +0100

    first commit

---
+++ exo1.js
@@ -0,0 +1,53 @@
+
+window.onload = function(){
+//Convertir en code ceci: Faire une fonction permettant de créer en une seul fois
+// les élements suivant: p, div, span,
+ //section et faire un console.log de ce qui a été crée
+ function creation(){
+    var creatediv1 = document.createElement('div');
+    var createP1 = document.createElement('p');
+    var createspan1 = document.createElement('span');
+     console.log(creatediv1 , createP1 , createspan1);
+ 
+    //return creatediv1 ,createspan1 , createP1 ;
+ }
+    console.log(creation('div','p','span'));
+
+    //methode 2
+ function elementHtml(){
+    var creatediv = document.createElement('div');
+    var createP = document.createElement('p');
+    var createspan = document.createElement('span');
+    var body = document.querySelector('body');
+    body.appendChild(creatediv);
+    creatediv.appendChild(createP);
+    createP.appendChild(createspan);
+      createspan.innerText = "hello world !";
+      
+        
+        return creatediv;    
+
+ }
+
+ elementHtml();
+
+ /*Convertir en code ceci: Faire une fonction qui va créer 3 élément html (div)
+ c'est trois div auront des attributs différent qui seront: id div1, id div2 et
+  id div3 ainsi qu'une class square pour chaque div créer plus une class color1, 
+  color2, color3, une fois fais l'afficher dans la console*/
+ var creatediv1 = [];
+    function repetition(){
+    for( var i=0 ; i< 3 ; i++){
+    creatediv1[i] = document.createElement('div');
+    creatediv1[i].innerText = "hello world !";
+    var body = document.querySelector('body');
+    body.appendChild(creatediv1[i]);
+    creatediv1[i].setAttribute('class', 'square');
+    creatediv1[i].setAttribute('id', 'Div' +(i+1));
+  } 
+  //console.log(i , creatediv)
+}   
+repetition();
+  
+  
+}
